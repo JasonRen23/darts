e5107593ef5db4a45b2ccc89e2a7f4bdab908741
JasonRen23 <zhicheng_ren@163.com>
2018-07-18 22:02:19 +0800

    :bulb:first commit

--- src/main/java/com/rzc/util/ValidateUtil.java
+++ src/main/java/com/rzc/util/ValidateUtil.java
@@ -1,9 +1,9 @@
-package util;
+package com.rzc.util;
 
 /**
  * @author : JasonRen
  * @date : 2018/07/18
  * @email : zhicheng_ren@163.com
  */
-public class ClassUtil {
+public class ValidateUtil {
 }
