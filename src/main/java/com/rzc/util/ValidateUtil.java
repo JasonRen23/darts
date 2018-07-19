a826b4ec626038db28b3f841d0bded2edba9fabf
JasonRen23 <zhicheng_ren@163.com>
2018-07-20 00:59:25 +0800

    :sparkles:add Validate Util Class

--- src/main/java/com/rzc/util/ValidateUtil.java
+++ src/main/java/com/rzc/util/ValidateUtil.java
@@ -1,9 +1,63 @@
 package com.rzc.util;
 
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * @author : JasonRen
  * @date : 2018/07/18
  * @email : zhicheng_ren@163.com
  */
 public class ValidateUtil {
+    /**
+     * 判断Object是否为null
+     * @param obj Object
+     * @return boolean
+     */
+    public static boolean isEmpty(Object obj){
+        return obj == null;
+    }
+    /**
+     * 判断String是否为null或“”
+     * @param obj Object
+     * @return boolean
+     */
+    public static boolean isEmpty(String obj){
+
+        return (obj == null || obj.equals(""));
+    }
+
+    public static boolean isEmpty(Object[] obj){
+        return obj == null || obj.length == 0;
+    }
+
+    public static boolean isEmpty(Collection<?> obj){
+        return obj == null || obj.isEmpty();
+    }
+
+    public static boolean isEmpty(Map<?,?> obj){
+        return obj == null || obj.isEmpty();
+    }
+
+    public static boolean isNotEmpty(Object obj){
+        return !isEmpty(obj);
+    }
+
+    public static boolean isNotEmpty(String obj){
+        return !isEmpty(obj);
+    }
+
+    public static boolean isNotEmpty(Object[] obj){
+        return !isEmpty(obj);
+    }
+
+    public static boolean isNotEmpty(Collection<?> obj){
+        return !isEmpty(obj);
+    }
+
+    public static boolean isNotEmpty(Map<?,?> obj){
+        return !isEmpty(obj);
+    }
+
+
 }
