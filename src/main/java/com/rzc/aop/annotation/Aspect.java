761f63419fcaf33fe5ed5d7dd8f526dc2871dfa3
JasonRen23 <zhicheng_ren@163.com>
2018-08-07 01:36:16 +0800

    :heavy_plus_sign:添加Aspect注解模板

---
+++ src/main/java/com/rzc/aop/annotation/Aspect.java
@@ -0,0 +1,16 @@
+package com.rzc.aop.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author JasonRen
+ * @since 2018/8/7 上午12:19
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Aspect {
+    /**
+     * 目标代理类的范围
+     */
+    Class<? extends Annotation> target();
+}
