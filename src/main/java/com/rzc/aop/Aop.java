e2fe9bb5d4021d799da8d62211009c2fab418253
JasonRen23 <zhicheng_ren@163.com>
2018-08-07 02:54:57 +0800

    :hammer:将创建代理通知类剥离出来

--- src/main/java/com/rzc/aop/Aop.java
+++ src/main/java/com/rzc/aop/Aop.java
@@ -21,21 +21,49 @@ public class Aop {
     }
 
     public void doAop() {
+        // beanContainer.getClassBySuper(Advice.class)
+        //     .stream()
+        //     .filter(clz -> clz.isAnnotationPresent(Aspect.class))
+        //     .forEach(clz -> {
+        //         final Advice advice = (Advice) beanContainer.getBean(clz);
+        //         Aspect aspect = clz.getAnnotation(Aspect.class);
+        //         beanContainer.getClassesByAnnotation(aspect.target())
+        //             .stream()
+        //             .filter(target -> !Advice.class.isAssignableFrom(target))
+        //             .filter(target -> !target.isAnnotationPresent(Aspect.class))
+        //             .forEach(target -> {
+        //                 ProxyAdvisor advisor = new ProxyAdvisor(advice);
+        //                 Object proxyBean = ProxyCreator.createProxy(target, advisor);
+        //                 beanContainer.addBean(target, proxyBean);
+        //             });
+        //     });
+
         beanContainer.getClassBySuper(Advice.class)
             .stream()
             .filter(clz -> clz.isAnnotationPresent(Aspect.class))
-            .forEach(clz -> {
-                final Advice advice = (Advice) beanContainer.getBean(clz);
-                Aspect aspect = clz.getAnnotation(Aspect.class);
-                beanContainer.getClassesByAnnotation(aspect.target())
+            .map(this::createProxyAdvisor)
+            .forEach(proxyAdvisor -> beanContainer.getClasses()
                     .stream()
                     .filter(target -> !Advice.class.isAssignableFrom(target))
                     .filter(target -> !target.isAnnotationPresent(Aspect.class))
                     .forEach(target -> {
-                        ProxyAdvisor advisor = new ProxyAdvisor(advice);
-                        Object proxyBean = ProxyCreator.createProxy(target, advisor);
-                        beanContainer.addBean(target, proxyBean);
-                    });
-            });
+                        if(proxyAdvisor.getPointCut().matches(target)) {
+                            Object proxtBean = ProxyCreator.createProxy(target, proxyAdvisor);
+                            beanContainer.addBean(target, proxtBean);
+                        }
+                    }));
+    }
+
+    /**
+     * 通过Aspect切面类创建代理通知类
+     * @param aspectClass 切面类
+     * @return 代理通知类
+     */
+    private ProxyAdvisor createProxyAdvisor(Class<?> aspectClass) {
+        String expression = aspectClass.getAnnotation(Aspect.class).pointCut();
+        ProxyPointCut proxyPointCut = new ProxyPointCut();
+        proxyPointCut.setExpression(expression);
+        Advice advice = (Advice) beanContainer.getBean(aspectClass);
+        return new ProxyAdvisor(advice, proxyPointCut);
     }
 }
