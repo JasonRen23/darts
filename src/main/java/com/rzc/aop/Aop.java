7070f951fd4a0e1d1dc0f27d5ab866c06a756205
JasonRen23 <zhicheng_ren@163.com>
2018-08-07 01:40:38 +0800

    :sparkles:实现代理类的主逻辑

---
+++ src/main/java/com/rzc/aop/Aop.java
@@ -0,0 +1,41 @@
+package com.rzc.aop;
+
+import com.rzc.aop.advice.Advice;
+import com.rzc.aop.annotation.Aspect;
+import com.rzc.core.BeanContainer;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author JasonRen
+ * @since 2018/8/7 上午12:35
+ */
+@Slf4j
+public class Aop {
+    /**
+     * Bean容器
+     */
+    private BeanContainer beanContainer;
+
+    public Aop() {
+        beanContainer = BeanContainer.getInstance();
+    }
+
+    public void doAop() {
+        beanContainer.getClassBySuper(Advice.class)
+            .stream()
+            .filter(clz -> clz.isAnnotationPresent(Aspect.class))
+            .forEach(clz -> {
+                final Advice advice = (Advice) beanContainer.getBean(clz);
+                Aspect aspect = clz.getAnnotation(Aspect.class);
+                beanContainer.getClassesByAnnotation(aspect.target())
+                    .stream()
+                    .filter(target -> !Advice.class.isAssignableFrom(target))
+                    .filter(target -> !target.isAnnotationPresent(Aspect.class))
+                    .forEach(target -> {
+                        ProxyAdvisor advisor = new ProxyAdvisor(advice);
+                        Object proxyBean = ProxyCreator.createProxy(target, advisor);
+                        beanContainer.addBean(target, proxyBean);
+                    });
+            });
+    }
+}
