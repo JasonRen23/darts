acb27e2ecb25b811809f5fa8a32d76748e23acbc
JasonRen23 <zhicheng_ren@163.com>
2018-08-07 01:44:00 +0800

    :sparkles:实现了一个用aspectj表达式来判定的切点类

---
+++ src/main/java/com/rzc/aop/ProxyPointCut.java
@@ -0,0 +1,93 @@
+package com.rzc.aop;
+
+import org.aspectj.weaver.tools.PointcutExpression;
+import org.aspectj.weaver.tools.PointcutParser;
+import org.aspectj.weaver.tools.PointcutPrimitive;
+import org.aspectj.weaver.tools.ShadowMatch;
+
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author JasonRen
+ * @since 2018/8/7 上午1:19
+ */
+public class ProxyPointCut {
+    /**
+     * 切点解析器
+     */
+    private PointcutParser pointcutParser;
+
+    /**
+     * （AspectJ）表达式
+     */
+    private String expression;
+
+    /**
+     * 表达式解析器
+     */
+    private PointcutExpression pointcutExpression;
+
+
+    private static final Set<PointcutPrimitive> DEFAULT_SUPPORTED_PRIMITIVES = new HashSet<>();
+
+    static {
+        DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.EXECUTION);
+        DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.ARGS);
+        DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.REFERENCE);
+        DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.THIS);
+        DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.TARGET);
+        DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.WITHIN);
+        DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.AT_ANNOTATION);
+        DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.AT_WITHIN);
+        DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.AT_ARGS);
+        DEFAULT_SUPPORTED_PRIMITIVES.add(PointcutPrimitive.AT_TARGET);
+    }
+
+    public ProxyPointCut() {
+        this(DEFAULT_SUPPORTED_PRIMITIVES);
+    }
+
+    public ProxyPointCut(Set<PointcutPrimitive> supportedPrimitives) {
+        pointcutParser = PointcutParser
+            .getPointcutParserSupportingSpecifiedPrimitivesAndUsingContextClassloaderForResolution(supportedPrimitives);
+    }
+
+    /**
+     * Class是否匹配切点表达式
+     */
+    public boolean matches(Class<?> targetClass) {
+        checkReadyToMatch();
+        return pointcutExpression.couldMatchJoinPointsInType(targetClass);
+    }
+
+    /**
+     * Method是否匹配切点表达式
+     */
+    public boolean matches(Method method) {
+        checkReadyToMatch();
+        ShadowMatch shadowMatch = pointcutExpression.matchesMethodExecution(method);
+        if (shadowMatch.alwaysMatches()) {
+            return true;
+        } else if (shadowMatch.neverMatches()) {
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * 初始化切点解析器
+     */
+    private void checkReadyToMatch() {
+        if (null == pointcutExpression) {
+            pointcutExpression = pointcutParser.parsePointcutExpression(expression);
+        }
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+
+
+}
